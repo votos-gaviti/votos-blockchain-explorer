ff0964addc5b5b7bbd3ee5a7e3f74c1c8f0a5956
Daniel <dani9573@gmail.com>
2023-08-14 18:41:45 -0500

    title issue

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -106,7 +106,7 @@ export class App extends React.Component<IAppProps> {
                                 translation={translation}
                                 userPreferences={this.props.userPreferences}
                             />
-                            <ToolbarItem title="Blockchain Medellín Decide" >
+                            <ToolbarItem title={translation.get("toolbar.alethio.label")} >
                                 <ExternalLink href="https://company.aleth.io" rel="noopener noreferrer">
                                     <ToolbarIconButton Icon={BriefcaseIcon} />
                                 </ExternalLink>
